318e6ccb06da49dd8cc6a7a8295ee5691321ff36
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-02-18 19:40:28 +0900

    chore(astro): update to astro v2 + add @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [image(), tailwind()]
 });
