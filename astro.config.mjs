5ec8c7605655b5d6cf2572503f6a229f59bee371
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-03-30 14:18:19 +0900

    feat(cookie): add cookie consent bar + button

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://avss2023.org",
   integrations: [image(), sitemap(), tailwind()],
 });
